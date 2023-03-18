0c5fba6f79f43b81c5a81f5d4d199436edf4f25c
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-18 22:29:31 +0100

    write function to fetch data

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 export default function Form() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=657b6c5e2a2ab2cafa267e54252ca1a7&language=de-GER&query=${search}`;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setMovies(data.results);
+        } else {
+          throw new Error("Something went wrong");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  console.log(movies);
+
   return (
     <>
       <form>
