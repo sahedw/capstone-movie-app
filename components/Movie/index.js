707efb64bd93c61b8317af0e7f79fb4a5c49c4aa
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-19 17:39:09 +0100

    use styled components to aligne search results with flexbox

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import { useContext } from "react";
 import { DataContext } from "../../pages/_app";
 import Image from "next/image";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  height: 180px;
+  width: 325px;
+  background-color: lightgray;
+`;
+
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
 
 export default function Movie() {
   const { movies } = useContext(DataContext);
@@ -9,14 +22,16 @@ export default function Movie() {
     <>
       {movies.map((movie) => {
         return (
-          <section key={movie.id}>
-            <Image
-              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-              alt={movie.title}
-              width={130}
-              height={180}
-            />
-          </section>
+          <StyledSection key={movie.id}>
+            <StyledDiv>
+              <Image
+                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                alt={movie.title}
+                width={130}
+                height={180}
+              />
+            </StyledDiv>
+          </StyledSection>
         );
       })}
     </>
