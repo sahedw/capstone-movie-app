cf2e4665cd37c56e8b957e551dfad35fe73f655a
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-19 21:23:25 +0100

    import image component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Form from "../components/Form";
 import { useContext } from "react";
 import { DataContext } from "./_app";
+import Image from "next/image";
 
 export default function Home() {
   const { handleFormSubmit, movies } = useContext(DataContext);
