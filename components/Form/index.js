795a6141f35da59c4eccb05688e3a16647ccaa28
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-20 10:35:45 +0100

    write function to direct user to search-results page if when search is submitted

--- components/Form/index.js
+++ components/Form/index.js
@@ -4,12 +4,18 @@ import { useRouter } from "next/router";
 export default function Form({ onSubmit }) {
   const router = useRouter();
 
+  function handleChangePage() {
+    router.push("/search-results");
+  }
+
   return (
     <>
       <form onSubmit={onSubmit}>
         <label htmlFor="search">Search a movie:</label>
         <input type="text" name="search" id="search" required />
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleChangePage}>
+          Submit
+        </button>
       </form>
     </>
   );
