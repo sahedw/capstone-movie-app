1b978664686c1385d91163b65f231d722b61c06c
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-20 11:02:02 +0100

    fix the component test with a mocked router

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,16 @@
 import Form from "../components/Form";
 import { render, screen } from "@testing-library/react";
 
-test("test", () => {
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      asPath: "/",
+    };
+  },
+}));
+
+test("Renders the correct label for my input in the form component", () => {
   render(<Form />);
   const element = screen.getByLabelText("Search a movie:");
   expect(element).toBeInTheDocument();
