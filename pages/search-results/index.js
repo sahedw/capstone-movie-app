f45ccf111e4bc06ef22fb8a9049eb3defabee8cd
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-20 10:44:28 +0100

    place backbutton component within the search-results page

--- pages/search-results/index.js
+++ pages/search-results/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Movie from "../../components/Movie";
+import BackButton from "../../components/BackButton";
 
 export default function SearchResultsPage() {
   return (
     <>
+      <BackButton />
       <section>
         <h2>Your search results:</h2>
       </section>
-      <section>
-        <Movie />
-      </section>
+      <Movie />
     </>
   );
 }
