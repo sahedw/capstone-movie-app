6a3e591fb77156d13c2bb2a77d577df9c047f213
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-20 10:32:31 +0100

    import useRouter

--- components/Form/index.js
+++ components/Form/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Form({ onSubmit }) {
+  const router = useRouter();
+
   return (
     <>
       <form onSubmit={onSubmit}>
