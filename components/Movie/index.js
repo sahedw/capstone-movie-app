e20db5c602bd498292275773f8ff6ec0f7193b59
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-20 13:15:45 +0100

    create array with all possible genres

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -4,6 +4,85 @@ import { DataContext } from "../../pages/_app";
 import Image from "next/image";
 import styled from "styled-components";
 
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Abenteuer",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Komödie",
+  },
+  {
+    id: 80,
+    name: "Krimi",
+  },
+  {
+    id: 99,
+    name: "Dokumentarfilm",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Familie",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "Historie",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Musik",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Liebesfilm",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV-Film",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "Kriegsfilm",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
 const StyledDiv = styled.div`
   height: 180px;
   width: 130px;
