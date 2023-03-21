0cdc3c9fa0f3454ef5d5bf9071eb1e329c0ee784
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-21 16:30:23 +0100

    display the movie detail content

--- components/MovieDetail/index.js
+++ components/MovieDetail/index.js
@@ -1,15 +1,54 @@
 import React from "react";
 import Image from "next/image";
+import getGenreFrom from "../../utils/getGenreFrom";
+import calculateRuntimeFrom from "../../utils/calculateRuntimeFrom";
+import { useState, useEffect } from "react";
 
 export default function MovieDetail({ movie }) {
+  const [runtime, setRuntime] = useState(0);
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movie.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=eng-US`
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setRuntime(data.runtime);
+          setMovieDetails(data);
+        } else {
+          throw new Error("Something went wrong");
+        }
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+      }
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+  console.log(movieDetails);
+
   return (
     <>
+      {/*       <Image
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt={movie.title}
+        width={600}
+        height={350}
+      /> */}
       <Image
         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt={movie.title}
         width={202.5}
         height={300}
       />
+      <p>{getGenreFrom(movie)}</p>
+      <h2>{movie.title} - </h2>
+      <p>{calculateRuntimeFrom(runtime)}</p>
+      <p>{movie.overview}</p>
     </>
   );
 }
