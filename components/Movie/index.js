677593f6b4bacd3bab1205bbb19c637a717ec56c
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-21 15:11:41 +0100

    make every mapped movie clickable so that it forwards the user to a dynamic route with its unique id

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { useState, useEffect } from "react";
 import calculateRuntimeFrom from "../../utils/calculateRuntimeFrom";
 import getGenreFrom from "../../utils/getGenreFrom";
+import Link from "next/link";
 
 const StyledDiv = styled.div`
   height: 180px;
@@ -14,6 +15,12 @@ const StyledSection = styled.section`
   display: flex;
   justify-content: center;
   margin-bottom: 20px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
 `;
 
 export default function Movie({ movie }) {
@@ -40,23 +47,25 @@ export default function Movie({ movie }) {
 
   return (
     <>
-      <StyledSection>
-        <StyledDiv>
-          <Image
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-            alt={movie.title}
-            width={130}
-            height={180}
-          />
-        </StyledDiv>
-        <section>
-          <h5>
-            {movie.title} - {movie.release_date.slice(0, 4)}
-          </h5>
-          <p>{getGenreFrom(movie)}</p>
-          <p>{calculateRuntimeFrom(runtime)}</p>
-        </section>
-      </StyledSection>
+      <StyledLink href={`search-results/${movie.id}`}>
+        <StyledSection>
+          <StyledDiv>
+            <Image
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.title}
+              width={130}
+              height={180}
+            />
+          </StyledDiv>
+          <section>
+            <h5>
+              {movie.title} - {movie.release_date.slice(0, 4)}
+            </h5>
+            <p>{getGenreFrom(movie)}</p>
+            <p>{calculateRuntimeFrom(runtime)}</p>
+          </section>
+        </StyledSection>
+      </StyledLink>
     </>
   );
 }
