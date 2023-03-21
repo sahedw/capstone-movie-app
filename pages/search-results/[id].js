a710cc0580ae3f97dfb197b9722b2c1388954946
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-21 16:01:50 +0100

    write return if currentMovie is unavailable or user types unavailable movie id in the url himself

--- pages/search-results/[id].js
+++ pages/search-results/[id].js
@@ -12,7 +12,13 @@ export default function MovieDetailPage() {
     (movie) => movie.id.toString() === router.query.id
   );
 
-  if (!currentMovie) return <h1>Sorry!</h1>;
+  if (!currentMovie)
+    return (
+      <>
+        <h1>We're quite sorry about this!</h1>
+        <p>{`The movie id ${router.query.id} seems to be not in out database.`}</p>
+      </>
+    );
 
   return (
     <>
