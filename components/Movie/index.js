cb206255a04ccfa7dcd657d36b70455013f2fbb3
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-21 10:42:22 +0100

    write function for runtime conversion in displayed hours and minutes

--- components/Movie/index.js
+++ components/Movie/index.js
@@ -16,7 +16,7 @@ const StyledSection = styled.section`
 `;
 
 export default function Movie({ movie }) {
-  const [runtime, setRuntime] = useState(null);
+  const [runtime, setRuntime] = useState(0);
 
   useEffect(() => {
     async function fetchData() {
@@ -57,6 +57,27 @@ export default function Movie({ movie }) {
     }
   }
 
+  function handleRuntimeConversion(runtime) {
+    if (runtime >= 180) {
+      const remaining = runtime % 180;
+      return `3h ${remaining}m`;
+    } else if (runtime <= 170 && runtime >= 121) {
+      const remaining = runtime % 120;
+      return `2h ${remaining}m`;
+    } else {
+      if (runtime <= 120 && runtime >= 61) {
+        const remaining = runtime % 60;
+        return `1h ${remaining}m`;
+      } else if (runtime <= 60 && runtime > 0) {
+        return `${runtime}m`;
+      } else {
+        return "no data";
+      }
+    }
+  }
+
+  console.log(runtime);
+
   return (
     <>
       <StyledSection key={movie.id}>
@@ -68,13 +89,12 @@ export default function Movie({ movie }) {
             height={180}
           />
         </StyledDiv>
-
         <section>
           <h5>
             {movie.title} - {movie.release_date.slice(0, 4)}
           </h5>
           <p>{handleRenderGenres(movie)}</p>
-          <p>{runtime}</p>
+          <p>{handleRuntimeConversion(runtime)}</p>
         </section>
       </StyledSection>
     </>
