4306303be7d2ab93d6b3ae3cf129fd844577ff31
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-22 12:07:02 +0100

    small fix in the showWatchProviders function

--- utils/showWatchProviders.js
+++ utils/showWatchProviders.js
@@ -1,7 +1,7 @@
 export default function showWatchProviders(streamingProvider) {
   if (streamingProvider?.length === 1) {
     return `Available on ${streamingProvider[0].provider_name}`;
-  } else if (streamingProvider?.length === 2) {
+  } else if (streamingProvider?.length > 1) {
     return `Available on ${streamingProvider[0].provider_name}, ${streamingProvider[1].provider_name}`;
   } else {
     return "Not available for streaming with a flatrate";
