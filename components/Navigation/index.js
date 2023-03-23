c420802d0e1aff29ae2ff98bc02df4dce3f25fe5
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-23 12:57:56 +0100

    create styled elements for footer/navbar

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  padding-inline-start: 0;
+`;
+
+const StyledNavBar = styled.nav``;
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+`;
 
 export default function Navigation() {
-  return <footer>Navigation</footer>;
+  return (
+    <StyledFooter>
+      <StyledNavBar>
+        <StyledList>
+          <li style={{ height: 70, width: "50%", backgroundColor: "beige" }}>
+            Home
+          </li>
+
+          <li style={{ height: 70, width: "50%", backgroundColor: "beige" }}>
+            Watchlist
+          </li>
+        </StyledList>
+      </StyledNavBar>
+    </StyledFooter>
+  );
 }
