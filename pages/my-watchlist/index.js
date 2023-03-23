76c0e171b34287625a50c16b5aad34c81c654660
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-23 10:09:50 +0100

    use movie component with content of watched state to map over the saved movies

--- pages/my-watchlist/index.js
+++ pages/my-watchlist/index.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import { WatchedContext } from "../_app";
+import Movie from "../../components/Movie";
 
 export default function MyWatchlistPage() {
-  return <></>;
+  const { watchedList } = useContext(WatchedContext);
+
+  return (
+    <>
+      <h2>My Watchlist:</h2>
+      <section>
+        {watchedList.map((movie) => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+      </section>
+    </>
+  );
 }
