1d3d236510cd7fff5453aad6aaabed493efba30d
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-23 14:20:08 +0100

    styled the nav icons, colors and proportions

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -5,8 +5,10 @@ import Link from "next/link";
 const StyledFooter = styled.footer`
   position: fixed;
   width: 100%;
-  height: 70px;
+  height: 80px;
   bottom: 0;
+  border-top: 1px solid lightgrey;
+  border-radius: 15px;
 `;
 
 const StyledNavBar = styled.nav``;
@@ -19,11 +21,28 @@ const StyledList = styled.ul`
 `;
 
 const StyledListItem = styled.li`
-  font-size: 30px;
+  font-size: 10px;
 `;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  color: black;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledParagraphIcon = styled.p`
+  margin: 0;
+  font-size: 25px;
+  text-align: center;
+`;
+
+const StyledParagraphText = styled.p`
+  margin: 0;
+  font-size: 15px;
 `;
 
 export default function Navigation() {
@@ -32,10 +51,20 @@ export default function Navigation() {
       <StyledNavBar>
         <StyledList>
           <StyledLink href={"/"}>
-            <StyledListItem>🔍</StyledListItem>
+            <StyledListItem>
+              <StyledDiv>
+                <StyledParagraphIcon>🔍</StyledParagraphIcon>
+                <StyledParagraphText>Search</StyledParagraphText>
+              </StyledDiv>
+            </StyledListItem>
           </StyledLink>
           <StyledLink href={"/my-watchlist"}>
-            <StyledListItem>📝</StyledListItem>
+            <StyledListItem>
+              <StyledDiv>
+                <StyledParagraphIcon>📝</StyledParagraphIcon>
+                <StyledParagraphText>Watchlist</StyledParagraphText>
+              </StyledDiv>
+            </StyledListItem>
           </StyledLink>
         </StyledList>
       </StyledNavBar>
