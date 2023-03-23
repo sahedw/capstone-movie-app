d0da7b975ff90acd24130a9e561cd492315f0f6c
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-23 10:22:23 +0100

    refactor the movie component and put the link outside of the component so its reusable for the watchlist with a different href. Also copied the moviedetail page with watchedlist state movies

---
+++ pages/my-watchlist/[id].js
@@ -0,0 +1,32 @@
+import React from "react";
+import MovieDetail from "../../components/MovieDetail";
+import { useContext } from "react";
+import { WatchedContext } from "../_app";
+
+import { useRouter } from "next/router";
+import BackButton from "../../components/PushButton";
+
+export default function MovieDetailPage() {
+  const { watchedList } = useContext(WatchedContext);
+
+  const router = useRouter();
+
+  const currentMovie = watchedList.find(
+    (movie) => movie.id.toString() === router.query.id
+  );
+
+  if (!currentMovie)
+    return (
+      <>
+        <BackButton name={"Back to Home"} route={"/"} />
+        <h1>{`We're quite sorry about this!`}</h1>
+        <p>{`The movie id ${router.query.id} seems to be not in your watchlist.`}</p>
+      </>
+    );
+
+  return (
+    <>
+      <MovieDetail movie={currentMovie} />
+    </>
+  );
+}
--- pages/my-watchlist/index.js
+++ pages/my-watchlist/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import { useContext } from "react";
 import { WatchedContext } from "../_app";
 import Movie from "../../components/Movie";
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+`;
 
 export default function MyWatchlistPage() {
   const { watchedList } = useContext(WatchedContext);
@@ -11,7 +19,11 @@ export default function MyWatchlistPage() {
       <h2>My Watchlist:</h2>
       <section>
         {watchedList.map((movie) => {
-          return <Movie key={movie.id} movie={movie} />;
+          return (
+            <StyledLink key={movie.id} href={`my-watchlist/${movie.id}`}>
+              <Movie movie={movie} />
+            </StyledLink>
+          );
         })}
       </section>
     </>
