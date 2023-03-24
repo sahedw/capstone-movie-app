29483a880fdf6e5aa30ff27efce52192bdabaee3
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-24 11:29:43 +0100

    delete unused code

--- utils/setCurrentNavIcon.js
+++ utils/setCurrentNavIcon.js
@@ -1,15 +1,3 @@
-/* export default function setCurrentNavIcon(router, whenCurrent, whenIdle, icon) {
-  if (router.asPath.match(whenCurrent)) {
-    return require(`/icons/${icon}-current.png`);
-  } else if (router.asPath.toString().length === 1) {
-    return require(`/icons/${icon}-current.png`);
-  } else if (router.asPath.match(whenIdle)) {
-    return require(`/icons/${icon}.png`);
-  } else {
-    return null;
-  }
-} */
-
 export default function setCurrentNavIcon(router, whenCurrent, whenIdle, icon) {
   if (router.asPath.toString().length === 1 && icon === "search") {
     return require(`/icons/${icon}-current.png`);
