e0941bd4ebdb5eace9074f8c95a59ebb3e19108d
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-27 10:12:13 +0200

    add content to cinema page

--- pages/cinema/index.js
+++ pages/cinema/index.js
@@ -1,5 +1,23 @@
 import React from "react";
+import Link from "next/link";
+import Navigation from "../../components/Navigation";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+`;
 
 export default function CinemaPage() {
-  return <div>CinemaPage</div>;
+  return (
+    <main>
+      <h2>Currently in cinemas:</h2>
+      {movies.map((movie) => (
+        <StyledLink key={movie.id} href={`search-results/${movie.id}`}>
+          <Movie movie={movie} />
+        </StyledLink>
+      ))}
+      <Navigation />
+    </main>
+  );
 }
