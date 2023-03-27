34f4f3617d72fb66d3ac1ee8e9ddd1687144d8ac
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-27 10:14:41 +0200

    write useeffect function to fetch data

--- pages/cinema/index.js
+++ pages/cinema/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Navigation from "../../components/Navigation";
+import { useEffect, useState } from "react";
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -8,7 +9,26 @@ const StyledLink = styled(Link)`
   cursor: pointer;
 `;
 
+const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`;
+
 export default function CinemaPage() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setMovies(data.results);
+        } else {
+          throw new Error("Something went wrong");
+        }
+      } catch (error) {
+        console.log(`Error: ${error.message}`);
+      }
+    }
+    fetchData();
+  }, [url]);
+
   return (
     <main>
       <h2>Currently in cinemas:</h2>
