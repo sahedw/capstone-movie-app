14de3b154db924f71b65694dab0b77e901e6a639
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-29 10:31:32 +0200

    create my-watched next js route with watched usestate

---
+++ pages/my-watched/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useContext } from "react";
+import { WatchedContext } from "../_app";
+import Movie from "../../components/Movie";
+import styled from "styled-components";
+import Link from "next/link";
+import Navigation from "../../components/Navigation";
+import { useRouter } from "next/router";
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+`;
+
+export default function MyWatchedPage() {
+  const { watched } = useContext(WatchedContext);
+
+  if (watched.length === 0)
+    return (
+      <main>
+        <h2>Nothing to 👀 here.</h2>
+        <p>Why dont you add some? </p>
+        <Navigation />
+      </main>
+    );
+
+  return (
+    <main>
+      <h2>My Watched movies:</h2>
+      <section>
+        {watched.map((movie) => {
+          return (
+            <StyledLink key={movie.id} href={`my-watched/${movie.id}`}>
+              <Movie movie={movie} />
+            </StyledLink>
+          );
+        })}
+      </section>
+      <Navigation />
+    </main>
+  );
+}
