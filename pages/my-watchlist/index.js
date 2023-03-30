191839358b94055064271a3bed23060db4d9357b
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-30 12:14:15 +0200

    create state for showing list or grid based on buttons

--- pages/my-watchlist/index.js
+++ pages/my-watchlist/index.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { WatchlistContext } from "../_app";
 import Movie from "../../components/Movie";
 import MovieGrid from "../../components/MovieGrid";
-
+import { useState } from "react";
 import styled from "styled-components";
 import Link from "next/link";
 import Navigation from "../../components/Navigation";
@@ -15,7 +15,12 @@ const StyledLink = styled(Link)`
 `;
 
 export default function MyWatchlistPage() {
+  const [listLayout, setListLayout] = useState(true);
   const { watchlist } = useContext(WatchlistContext);
+
+  function setLayoutType(boolean) {
+    setListLayout(boolean);
+  }
 
   if (watchlist.length === 0)
     return (
@@ -29,6 +34,22 @@ export default function MyWatchlistPage() {
   return (
     <main>
       <h2>My Watchlist ({watchlist.length}):</h2>
+      <button
+        onClick={() => {
+          setLayoutType(true);
+        }}
+        disabled={listLayout ? true : false}
+      >
+        List
+      </button>
+      <button
+        onClick={() => {
+          setLayoutType(false);
+        }}
+        disabled={listLayout ? false : true}
+      >
+        Grid
+      </button>
       <section>
         {watchlist.map((movie) => {
           return (
