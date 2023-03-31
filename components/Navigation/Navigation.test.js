f6a78e5339e5f6a54d33e59223ed64964229a1d2
Sahed Wahedi <sahed.w@hotmail.de>
2023-03-31 13:34:11 +0200

    fix tests by providing new context

--- components/Navigation/Navigation.test.js
+++ components/Navigation/Navigation.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Navigation from ".";
+import { DataContext } from "../../pages/_app";
+import { useContext } from "react";
 
 jest.mock("next/router", () => ({
   useRouter() {
@@ -10,8 +12,14 @@ jest.mock("next/router", () => ({
   },
 }));
 
+const theme = "light";
+
 test("Should show the search text in 'current' color because highlighted with mock router", () => {
-  render(<Navigation />);
+  render(
+    <DataContext.Provider value={{ theme }}>
+      <Navigation />
+    </DataContext.Provider>
+  );
   const element = screen.getByText("Search");
   expect(element).toHaveStyle("color: #F97B7B;");
   expect(element).toHaveAttribute("value");
