d6ae50eb98fc0590f08a58393051cd3ada8c0c5e
Sahed Wahedi <sahed.w@hotmail.de>
2023-04-03 11:04:53 +0200

    add function to handle upcoming movies and currently in cinema movies

--- pages/cinema/[id].js
+++ pages/cinema/[id].js
@@ -7,13 +7,27 @@ import PushButton from "../../components/PushButton";
 import Navigation from "../../components/Navigation";
 
 export default function MovieDetailPage() {
-  const { currentlyInCinemas } = useContext(CinemaContext);
+  const { currentlyInCinemas, upcomingMovies } = useContext(CinemaContext);
 
   const router = useRouter();
 
-  const currentMovie = currentlyInCinemas.find(
-    (movie) => movie.id.toString() === router.query.id
-  );
+  function getCinemaMovie() {
+    if (
+      currentlyInCinemas.find(
+        (movie) => movie.id.toString() === router.query.id
+      )
+    ) {
+      return currentlyInCinemas.find(
+        (movie) => movie.id.toString() === router.query.id
+      );
+    } else {
+      return upcomingMovies.find(
+        (movie) => movie.id.toString() === router.query.id
+      );
+    }
+  }
+
+  const currentMovie = getCinemaMovie();
 
   if (!currentMovie)
     return (
