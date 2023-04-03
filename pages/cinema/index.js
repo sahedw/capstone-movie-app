14b9cbe5fddc6c32483a5ff20d6fe46a78cda442
Sahed Wahedi <sahed.w@hotmail.de>
2023-04-03 10:39:28 +0200

    write the logic for button to switch with state and styling

--- pages/cinema/index.js
+++ pages/cinema/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import Link from "next/link";
 import Navigation from "../../components/Navigation";
 import { useContext } from "react";
-import { CinemaContext } from "../_app";
+import { CinemaContext, DataContext } from "../_app";
 import MovieGrid from "../../components/MovieGrid";
 import styled from "styled-components";
+import { useState } from "react";
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -23,8 +24,45 @@ const StyledSection = styled.section`
   padding-right: 15px;
 `;
 
+const StyledSectionButtonsFlex = styled.section`
+  width: 100%;
+`;
+
+const StyledSectionButtons = styled.section`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledButtonList = styled.button`
+  border: none;
+  background-color: transparent;
+
+  :enabled {
+    color: ${(props) => props.theme.fontColor};
+  }
+
+  :disabled {
+    color: #f97b7b;
+  }
+`;
+
+const StyledButtonGrid = styled.button`
+  border: none;
+  background-color: transparent;
+
+  :enabled {
+    color: ${(props) => props.theme.fontColor};
+  }
+
+  :disabled {
+    color: #f97b7b;
+  }
+`;
+
 export default function CinemaPage() {
   const { currentlyInCinemas } = useContext(CinemaContext);
+  const { theme } = useContext(DataContext);
+  const [showPlayingInCinema, setShowPlayingInCinema] = useState(true);
 
   if (!currentlyInCinemas) return <h1>Loading...</h1>;
 
@@ -32,6 +70,28 @@ export default function CinemaPage() {
     <main>
       <StyledSection>
         <h2>Currently in cinemas:</h2>
+        <StyledSectionButtonsFlex>
+          <StyledSectionButtons>
+            <StyledButtonList
+              color={theme}
+              onClick={() => {
+                setLayoutType(true);
+              }}
+              disabled={showPlayingInCinema ? true : false}
+            >
+              Playing
+            </StyledButtonList>
+            <StyledButtonGrid
+              color={theme}
+              onClick={() => {
+                setLayoutType(false);
+              }}
+              disabled={showPlayingInCinema ? false : true}
+            >
+              Upcoming
+            </StyledButtonGrid>
+          </StyledSectionButtons>
+        </StyledSectionButtonsFlex>
         <StyledDiv>
           {currentlyInCinemas.map((movie) => (
             <StyledLink key={movie.id} href={`cinema/${movie.id}`}>
