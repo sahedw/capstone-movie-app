02a6ea274d7494ca2db5eddf962a90dbb5fc29ca
Sahed Wahedi <sahed.w@hotmail.de>
2023-04-06 12:41:52 +0200

    add logic to conditionally load detail page of movie or tv depending on which poster is clicked

--- pages/my-watchlist/[id].js
+++ pages/my-watchlist/[id].js
@@ -1,20 +1,27 @@
 import React from "react";
 import MovieDetail from "../../components/MovieDetail";
 import { useContext } from "react";
-import { WatchlistContext } from "../_app";
+import { WatchlistContext, WatchlistTVContext } from "../_app";
 import { useRouter } from "next/router";
 import PushButton from "../../components/PushButton";
 import MovieDetailFooter from "../../components/MovieDetailFooter";
 import Navigation from "../../components/Navigation";
+import TVDetail from "../../components/TVDetail";
+import TVDetailFooter from "../../components/TVDetailFooter";
 
 export default function MovieDetailPage() {
   const { watchlist } = useContext(WatchlistContext);
+  const { watchlistTV } = useContext(WatchlistTVContext);
 
   const router = useRouter();
 
-  const currentMovie = watchlist.find(
+  /*  const currentMovie = watchlist.find(
     (movie) => movie.id.toString() === router.query.id
-  );
+  ); */
+
+  const currentMovie =
+    watchlist.find((movie) => movie.id.toString() === router.query.id) ||
+    watchlistTV.find((movie) => movie.id.toString() === router.query.id);
 
   if (!currentMovie)
     return (
@@ -28,8 +35,17 @@ export default function MovieDetailPage() {
 
   return (
     <main>
-      <MovieDetail movie={currentMovie} />
-      <MovieDetailFooter movie={currentMovie} />
+      {watchlist.includes(currentMovie) ? (
+        <>
+          <MovieDetail movie={currentMovie} />
+          <MovieDetailFooter movie={currentMovie} />
+        </>
+      ) : (
+        <>
+          <TVDetail movie={currentMovie} />
+          <TVDetailFooter movie={currentMovie} />
+        </>
+      )}
     </main>
   );
 }
