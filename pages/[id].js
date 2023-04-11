8cdf18beab7a60dfb646fafb85c3477f6de53ae0
Sahed Wahedi <sahed.w@hotmail.de>
2023-04-11 12:10:22 +0200

    fix

--- pages/[id].js
+++ pages/[id].js
@@ -16,9 +16,9 @@ export default function MovieDetailPage() {
     if (watchlist.find((movie) => movie.id.toString() === router.query.id)) {
       return watchlist.find((movie) => movie.id.toString() === router.query.id);
     } else if (
-      trendingMovies.find((movie) => movie.id.toString() === router.query.id)
+      trendingMovies?.find((movie) => movie.id.toString() === router.query.id)
     ) {
-      return trendingMovies.find(
+      return trendingMovies?.find(
         (movie) => movie.id.toString() === router.query.id
       );
     } else {
